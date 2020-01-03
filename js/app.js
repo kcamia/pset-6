05fe4a694d0a548da176b2eef2cc7ef3f2d824e0
Kristina Camia <kcamia19@gmail.com>
2020-01-03 13:10:53 -0500

    add item

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 window.onload = function() {
-  document.getElementById("Add").onclick = addItem;
+  document.getElementById("add").onclick = addItem;
 }
 
+let elements = [];
+
 const addItem = function() {
-  console.log("hi");
+  let newItem = document.getElementById("item").value;
+  elements.push(newItem);
+  let listItem = document.createElement("LI");
+  listItem.innerHTML = newItem;
+  document.body.appendChild(listItem);
 }
